1cfa82a2f4a32a97d65e1267a6a061e3125faa42
Ahmad-Saadeh <Ahmad-Saadeh@users.noreply.github.com>
2020-03-12 11:37:33 +0200

    RJS4

---
+++ src/AuthorDetail.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+
+class AuthorDetail extends Component {
+
+
+    books = this.props.author.books.map(book => (
+
+        <tr key={book.title}>
+            <td>{book.title}</td>
+            <td>{` ${this.props.author.first_name} ${this.props.author.last_name}`}</td>
+            <td>
+                <button className="btn" style={{ backgroundColor: book.color }} />
+            </td>
+        </tr>
+
+    ));
+
+    render() {
+        return (
+            <div className="author col-xs-10">
+                <div>
+                    <h3>{this.props.author.first_name} {this.props.author.last_name}</h3>
+                    <img src={this.props.author.imageUrl} className="img-thumbnail" />
+                </div>
+                <table className='mt-3 table'>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Authors</th>
+                            <th>Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {this.books}
+
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+export default AuthorDetail;
